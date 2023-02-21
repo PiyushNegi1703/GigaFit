71e9b09a62780289273f238d4bbac4b041d5e3f1
Uttam Negi <piyush.negi@kalvium.community>
2023-02-21 14:15:28 +0530

    Added Navbar and a page for workouts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
-import './index.css';
-import Home from './pages/Home';
-import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/LoginPage';
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import TestPage from "./workoutPages/TestPage";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<LandingPage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/home' element={<Home />} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/workout" element={<TestPage />} />
     </Routes>
   );
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,118 +1,113 @@
 import React from "react";
 import Tilt from "react-parallax-tilt";
-import logo from "../assets/nav-logo.png";
-import { CgProfile } from "react-icons/cg";
 import { AiFillStar } from "react-icons/ai";
 import data from "../data/data";
-import fullBody from "../assets/Full bode workout.png"
+import fullBody from "../assets/Full bode workout.png";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="home">
-      <nav
-        style={{
-          height: "10vh",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          paddingRight: "40px",
-          background: "#ca0024",
-          position: "sticky",
-        }}
-      >
-        <img src={logo} alt="" style={{ width: "200px" }} />
+    <>
+      <Navbar />
+      <div className="home">
+        <div className="suggestion">
+          <h1
+            style={{
+              color: "#fff",
+              textDecoration: "underline solid 1px",
+              textAlign: "center",
+              textShadow: "0 0 15px #ca0024",
+            }}
+          >
+            OUR SUGGESTION FOR YOU
+          </h1>
 
-        <CgProfile style={{ fontSize: "2.5em" }} />
-      </nav>
+          <Tilt
+            glareEnable={true}
+            glarePosition={"all"}
+            tiltMaxAngleX={5}
+            tiltMaxAngleY={5}
+            className="suggestion-card"
+          >
+            <Link to="/workout">
+              <img src={fullBody} alt="" />
+              <h2 style={{ fontSize: "2em", letterSpacing: "2px" }}>
+                Full Body Workout Challenge
+              </h2>
+              <h4 style={{ fontSize: "1.2em", letterSpacing: "2px" }}>
+                4 days x 6 weeks
+              </h4>
+            </Link>
+          </Tilt>
+        </div>
 
-      <div className="suggestion">
-        <h1
-          style={{
-            color: "#fff",
-            textDecoration: "underline solid 1px",
-            textAlign: "center",
-            textShadow: "0 0 15px #ca0024",
-          }}
-        >
-          OUR SUGGESTION FOR YOU
-        </h1>
+        {/* WORKOUTS CONTAINER */}
+        <div className="workouts" style={{ marginBottom: "5vh" }}>
+          {/* BEGINNER WORKOUTS */}
+          <h1 style={{ margin: "2vh 0 2vh 7%" }}>Beginner Workouts</h1>
+          <div className="card-container">
+            {data.map((e) => {
+              return (
+                <Tilt glareEnable={true} glarePosition={"all"} className="card">
+                  <Link to="/workout">
+                    <img src={e.image} alt="" />
+                    <h2>{e.title} Beginner</h2>
+                    <div className="rating">
+                      <AiFillStar className="star" color="#ff385c" />
+                      <AiFillStar className="star" />
+                      <AiFillStar className="star" />
+                    </div>
+                  </Link>
+                </Tilt>
+              );
+            })}
+          </div>
 
-        <Tilt
-          glareEnable={true}
-          glarePosition={"all"}
-          tiltMaxAngleX={5}
-          tiltMaxAngleY={5}
-          className="suggestion-card"
-        >
-          <img src={fullBody} alt="" />
-          <h3 style={{ fontSize: "1.5em", letterSpacing: "2px" }}>
-            Full Body Workout Challenge
-          </h3>
-          <h5 style={{ fontSize: "1.2em", letterSpacing: "2px" }}>
-            4 days x 6 weeks
-          </h5>
-        </Tilt>
+          {/* INTERMEDIATE WORKOUTS */}
+          <h1 style={{ margin: "2vh 0 2vh 7%" }}>Intermediate Workouts</h1>
+          <div className="card-container">
+            {data.map((e) => {
+              return (
+                <Tilt glareEnable={true} glarePosition={"all"} className="card">
+                  <Link to="/workout">
+                    <img src={e.image} alt="" />
+                    <h2>{e.title} Intermediate</h2>
+                    <div className="rating">
+                      <AiFillStar className="star" color="#ff385c" />
+                      <AiFillStar className="star" color="#ff385c" />
+                      <AiFillStar className="star" />
+                    </div>
+                  </Link>
+                </Tilt>
+              );
+            })}
+          </div>
+
+          {/* ADVANCED WORKOUTS */}
+          <h1 style={{ margin: "2vh 0 2vh 7%" }}>Advanced Workouts</h1>
+          <div className="card-container">
+            {data.map((e) => {
+              return (
+                <Tilt glareEnable={true} glarePosition={"all"} className="card">
+                  <Link to="/workout">
+                    <img src={e.image} alt="" />
+                    <h2>{e.title} Advanced</h2>
+                    <div className="rating">
+                      <AiFillStar className="star" color="#ff385c" />
+                      <AiFillStar className="star" color="#ff385c" />
+                      <AiFillStar className="star" color="#ff385c" />
+                    </div>
+                  </Link>
+                </Tilt>
+              );
+            })}
+          </div>
+        </div>
+
+        <footer>Hello</footer>
       </div>
-
-      {/* WORKOUTS CONTAINER */}
-      <div className="workouts" style={{marginBottom: '5vh'}}>
-        {/* BEGINNER WORKOUTS */}
-        <h1 style={{ margin: "2vh 0 2vh 7%" }}>Beginner Workouts</h1>
-        <div className="card-container">
-          {data.map((e) => {
-            return (
-              <Tilt glareEnable={true} glarePosition={"all"} className="card">
-                <img src={e.image} alt="" />
-                <h2>{e.title} Beginner</h2>
-                <div className="rating">
-                  <AiFillStar className="star" color="#ff385c" />
-                  <AiFillStar className="star" />
-                  <AiFillStar className="star" />
-                </div>
-              </Tilt>
-            );
-          })}
-        </div>
-
-        {/* INTERMEDIATE WORKOUTS */}
-        <h1 style={{ margin: "2vh 0 2vh 7%" }}>Intermediate Workouts</h1>
-        <div className="card-container">
-          {data.map((e) => {
-            return (
-              <Tilt glareEnable={true} glarePosition={"all"} className="card">
-                <img src={e.image} alt="" />
-                <h2>{e.title} Intermediate</h2>
-                <div className="rating">
-                  <AiFillStar className="star" color="#ff385c" />
-                  <AiFillStar className="star" color="#ff385c" />
-                  <AiFillStar className="star" />
-                </div>
-              </Tilt>
-            );
-          })}
-        </div>
-
-        {/* ADVANCED WORKOUTS */}
-        <h1 style={{ margin: "2vh 0 2vh 7%" }}>Advanced Workouts</h1>
-        <div className="card-container">
-          {data.map((e) => {
-            return (
-              <Tilt glareEnable={true} glarePosition={"all"} className="card">
-                <img src={e.image} alt="" />
-                <h2>{e.title} Advanced</h2>
-                <div className="rating">
-                  <AiFillStar className="star" color="#ff385c" />
-                  <AiFillStar className="star" color="#ff385c" />
-                  <AiFillStar className="star" color="#ff385c" />
-                </div>
-              </Tilt>
-            );
-          })}
-        </div>
-      </div>
-
-      <footer>Hello</footer>
-    </div>
+    </>
   );
 };
 
